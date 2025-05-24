9437eabcb604bd74c21973cb8a79068d3173b695
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-05-24 15:06:52 +0530

    change css

--- src/view/pages/Contact/Contact.tsx
+++ src/view/pages/Contact/Contact.tsx
@@ -14,7 +14,6 @@ export function Contact() {
     const onSubmit = (data: FormData) => {
         console.log("Form data submitted:", data);
         alert(`Submitted form data.`);
-        $
     }
 
     return (
@@ -23,24 +22,28 @@ export function Contact() {
             <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name" {...register("name", {required: "Name is required", pattern: {value: /^[A-Za-z\s]+$/, message: "Name must contain only letters and spaces"}})} />
-
+                {errors.name ? (
+                    <span className="error">{errors.name.message}</span>
+                ) : ''}
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" {...register("email", {required: "Email is required"})} />
+                <input type="email" id="email" {...register("email", {required: "Email is required",pattern: {value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i, message: "Invalid email format"}})} />
+                {errors.email ? (
+                    <span className="error">{errors.email.message}</span>
+                ) : ''}
 
                 <label htmlFor="message">Message:</label>
-                <textarea id="message" {...register("message", {required:true})} rows={4}> </textarea>
+                <textarea
+                    id="message"
+                    rows={4}
+                    {...register("message", { required: true })}
+                />
+                {errors.message && (
+                    <span className="error">Message is required</span>
+                )}
 
                 <button type="submit">Send</button>
             </form>
-            {errors.name ? (
-                <span className="error">{errors.name.message}</span>
-            ) : ''}
-            {errors.email ? (
-                <span className="error">{errors.email.message}</span>
-            ) : ''}
-            {errors.message ? (
-                <span className="error">{errors.message.message}</span>
-            ) : ''}
+
         </div>
     );
 }
