c20f0ff4a0607ea09f18e08ac26bdc8ac752c0f9
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-05-24 10:24:58 +0530

    change css

--- src/view/common/Navbar/Navbar.tsx
+++ src/view/common/Navbar/Navbar.tsx
@@ -1,11 +1,14 @@
 import "./Navbar.css";
+import {Link} from "react-router-dom";
 
 export function Navbar() {
     return (
         <ul className="navbar">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>
+                <Link to={"/"}>Home</Link>
+            </li>
+            <li><Link to={"/about"}>About</Link></li>
+            <li><Link to={"/contact"}>Contact</Link></li>
         </ul>
     );
 }
