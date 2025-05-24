12c730e6335a356d1cdf1414b3e0d65b1079ee1a
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-05-24 09:26:34 +0530

    update css

--- src/view/common/MainContent/MainContent.tsx
+++ src/view/common/MainContent/MainContent.tsx
@@ -1,6 +1,6 @@
 import "./MainContent.css";
 export function MainContent() {
     return (
-        <div className="main-content"> This is MainContent</div>
+        <div className="main-content"> This is Main Content</div>
     );
 }
