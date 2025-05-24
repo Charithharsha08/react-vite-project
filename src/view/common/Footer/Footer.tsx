16740353f3f62aa2cb4ab3cc37c60d6da5f39e20
Charith Harsha <147936344+Charithharsha08@users.noreply.github.com>
2025-05-24 09:57:19 +0530

    add dynamically page navigation

---
+++ src/view/common/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2025 Charith Harsha. All rights reserved.</p>
+        </footer>
+    );
+}
--- src/view/common/MainContent/MainContent.tsx
+++ src/view/common/MainContent/MainContent.tsx
@@ -1,6 +1,10 @@
 import "./MainContent.css";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "../../pages/Home/Home.tsx";
 export function MainContent() {
     return (
-        <div className="main-content"> This is Main Content</div>
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        </Routes>
     );
 }
